c44dcb4d216b2e5b5a663f161231ffc36254872d
Florian <florian.favre-fiurino@laposte.net>
2022-12-12 16:51:54 +0100

    travail sur market_depth

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
 
 class Market
   attr_accessor :bids, :base, :quote, :asks
@@ -7,44 +9,87 @@ class Market
     @quote = "EUR"
     @bids = []
     @asks = []
-    @orders = []
   end
 
   def submit(order)
-    @orders << order
-    if order[3] == 1
-      @bids.append([order[2].to_f, order[1].truncate(8)])
-    else
-      @asks.append([order[2].to_f, order[1].truncate(8)])
+    if order["side"] == 1
+      @bids.append([order["price"], order["amount"]])
+    elsif order["side"] == 2
+      @asks.append([order["price"], order["amount"]])
     end
-    return "Order number #{order[0]} submitted"
+    return "\n Order number #{order["order_id"]} submitted \n"
   end
 
   def market_price
-    #make an average between the best bid and the best ask
-    return (@bids[0][0] + @asks[0][0]) / 2
+    if(@bids.empty? || @asks.empty?)
+      return "\n No market price \n"
+    else
+      m_price_bids = 0
+      m_price_asks = 0
+      @bids.each do |bid|
+        if bid[0] > m_price_bids
+          m_price_bids = bid[0]
+        end
+      end
+      @asks.each do |ask|
+        if ask[0] < m_price_asks
+          m_price_asks = ask[0]
+        end
+      end
+      calc = (m_price_bids + m_price_asks) / 2      
+      return sprintf( "%.02f", calc)
 
+    end
   end
 
   def market_depth
-    return "bids: #{@bids} \n
-    base: #{@base} \n
-    quote: #{@quote} \n
-    asks: #{@asks}"
+    if(@bids.empty? || @asks.empty?)
+      return "\n No market price \n"
+    else
+      depth =  "\n {\"bids\"=> \n["
+      #parcours les bids et transforme la colonne price en float, la colonne amount en décimale avec huit chiffres après la virgule
+      @bids.each do |bid|
+        depth += "[\"#{sprintf( "%.02f", bid[0])}\", \"#{sprintf( "%.08f", bid[1])}\"],\n"
+      end
+      depth -= ",\n"
+      depth += "]\n base=>\"#{@base}\", \n"
+      depth += "quote=>\"#{@quote}\", \n"
+      #parcours les asks et transforme la colonne price en float, la colonne amount en décimale avec huit chiffres après la virgule
+      depth += "\"asks\"=> \n["
+      @asks.each do |ask|
+        depth += "[\"#{sprintf( "%.02f", ask[0])}\", \"#{sprintf( "%.08f", ask[1])}\"],\n"
+      end
+      depth -= ",\n"
+      puts depth += "]}\n"
+    end
   end
 
   def cancel_order(id)
-    side = @orders[id]
-    if side == 1
-      @bids.delete_at(id)
+
+    if(@bids.empty? || @asks.empty?)
+      return "\n No order to cancel \n"
     else
-      @asks.delete_at(id)
+      side = 0
+      @bids.each do |bid|
+        if(bid[id] != nil)
+          side = 1
+          @bids.delete_at(bid)
+        end
+      end
+      if side == 0
+        @asks.each do |ask|
+          if(ask[id] != nil)
+            @asks.delete_at(ask)
+          end
+        end
+      end
+      puts "Order number #{id} canceled"
+      puts "bids: #{@bids} \n
+      base: #{@base} \n
+      quote: #{@quote} \n
+      asks: #{@asks}"
+
     end
-    @orders.delete_at(id)
-    puts "Order number #{id} canceled"
-    puts "bids: #{@bids} \n
-    base: #{@base} \n
-    quote: #{@quote} \n
-    asks: #{@asks}"
+    
   end
 end
--- main.rb
+++ main.rb
@@ -3,53 +3,60 @@ require 'bigdecimal'
 
 market = Market.new
 order_id = 1
+action = 0
 
-puts "You want to trade on the BTC/EUR market."
-puts "Create your first order: press enter to continue."
-if gets.chomp != ""
-  puts "No problem, you can create your first order later."
-  exit
-else
-  puts "Transaction amount in BTC:"
-  amount = BigDecimal(gets.chomp)
-  puts "Transaction price in EUR:"
-  price = BigDecimal(gets.chomp)
-  puts "Buy (1) or sell (2):"
-  side = gets.chomp.to_i
-
-  order_a = [order_id, amount, price, side]
-  order_id = order_id + 1
-  market.submit(order_a)
-  puts "You want to create a second order: press enter to continue."
-  if gets.chomp != ""
-    puts "It's ok you can create your second order later."
-    exit
-  else
+while action != 5
+  puts "\n *************************** \n"
+  puts "You want to trade on the BTC/EUR market. Choose your action"
+  puts "Create order of trade number #{order_id}: press 1"
+  puts "View the market depth: press 2"
+  puts "View the Market price: press 3"
+  puts "Cancel an order: press 4"
+  puts "Exit: press 5"
+  action = gets.chomp.to_i
+  if action == 1
     puts "Transaction amount in BTC:"
     amount = BigDecimal(gets.chomp)
     puts "Transaction price in EUR:"
     price = BigDecimal(gets.chomp)
     puts "Buy (1) or sell (2):"
     side = gets.chomp.to_i
-    order_b = [order_id, amount, price, side]
+
+    order_a = {
+      "order_id" => order_id,
+      "amount" => amount,
+      "price" => price,
+      "side" => side
+    }
     order_id = order_id + 1
-    market.submit(order_b)
-
+    market.submit(order_a)
   end
+
+  if action == 2
+    puts "Market depth:"
+    puts market.market_depth
+  end
+
+  if action == 3
+    puts "Market price:"
+    puts market.market_price
+  end
+
+  if action == 4
+    puts "You want to cancel an order: press id of order you want to cancel."
+    num_order_cancel = gets.chomp.to_i
+    if num_order_cancel != 1 && num_order_cancel != 2
+      puts "No order was canceled"
+      exit
+    else
+      puts "Cancel order #{num_order_cancel}:"
+      market.cancel_order(num_order_cancel)
+    end
+  end
+
+  if action == 5
+    exit
+  end
+
 end
 
-puts "Market depth:"
-puts market.market_depth
-
-puts "Market price:"
-puts market.market_price
-
-puts "You want to cancel an order: press number to cancel it."
-num_order_cancel = gets.chomp.to_i
-if num_order_cancel != 1 && num_order_cancel != 2
-  puts "No order was canceled"
-  exit
-else
-  puts "Cancel order #{num_order_cancel}:"
-  market.cancel_order(num_order_cancel)
-end
